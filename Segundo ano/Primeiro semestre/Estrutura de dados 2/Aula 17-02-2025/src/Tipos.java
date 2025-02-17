454293c77829a48fa15ba56373d1b09b2df2cc26
GabrielFerrarez19 <gabrielferrarez190206@gmail.com>
2025-02-17 20:20:22 -0300

    Aula 17/02/2025

---
+++ Segundo ano/Primeiro semestre/Estrutura de dados 2/Aula 17-02-2025/src/Tipos.java
@@ -0,0 +1,12 @@
+public class Tipos {
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        Byte numero = 127;
+        System.out.println("---teste---");
+        System.out.println(numero);
+        //numero = numero++;
+        //System.out.println(numero++);
+        System.out.println(++numero);
+    }
+}
