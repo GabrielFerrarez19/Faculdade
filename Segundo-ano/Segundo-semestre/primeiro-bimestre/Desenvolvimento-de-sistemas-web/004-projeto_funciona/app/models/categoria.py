10cdae40db8a2c07720e69ae104445b2686c1157
GabrielFerrarez19 <gabrielferrarez190206@gmail.com>
2025-09-24 19:50:57 -0300

    aula Alysson

--- Segundo-ano/Segundo-semestre/primeiro-bimestre/Desenvolvimento-de-sistemas-web/004-projeto_funciona/app/models/categoria.py
+++ Segundo-ano/Segundo-semestre/primeiro-bimestre/Desenvolvimento-de-sistemas-web/004-projeto_funciona/app/models/categoria.py
@@ -1,11 +1,12 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-from app.db.session import Base
+from app.db.base import Base
 
 class Categoria(Base):
     __tablename__ = "categorias"
     id = Column(Integer, primary_key=True, index=True)
     nome = Column(String)
-    descricao = Column(String)
-
-    produtos = relationship("Produto", back_populates="categoria", cascade="all, delete-orphan")
+    produtos = relationship(
+        "Produto", 
+        back_populates="categoria",
+        cascade="all, delete-orphan")
